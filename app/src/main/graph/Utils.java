ab8ce4948b881c59e262315dc96da0b396defc87
Mohammad Mahdi Soozandeh <mo.m10003@gmail.com>
2021-01-13 16:01:55 +0330

    minor improvement

--- app/src/main/graph/Utils.java
+++ app/src/main/graph/Utils.java
@@ -4,7 +4,7 @@ import javafx.scene.shape.Circle;
 
 public class Utils {
 
-    // Linear equation in the X-Y Axis: y = Ax + B
+    // A of Linear equation in the X-Y Axis: y = Ax + B
     public static double calculateAofLine(Vertex point1, Vertex point2) {
         double x1 = point1.getCircle().getCenterX();
         double y1 = point1.getCircle().getCenterY();
@@ -14,7 +14,7 @@ public class Utils {
         return (y1 - y2) / (x1 - x2);
     }
 
-    // Linear equation in the X-Y Axis: y = Ax + B
+    // B of Linear equation in the X-Y Axis: y = Ax + B
     public static double calculateBofLine(Vertex point, double mA) {
         double x1 = point.getCircle().getCenterX();
         double y1 = point.getCircle().getCenterY();
@@ -22,12 +22,14 @@ public class Utils {
         return y1 - (mA * x1);
     }
 
+    // calculate distance of two vertex base on X-Y Coordination
     public static double distance(Vertex point1, Vertex point2) {
         double x = point1.getCircle().getCenterX() - point2.getCircle().getCenterX();
         double y = point1.getCircle().getCenterY() - point2.getCircle().getCenterY();
         return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
 
+    // return a vertex where two lines meet
     public static Vertex theIntersectionPointOfTwoLine(double a, double b, double aPrime, double bPrime) {
         if (a - aPrime == 0) return null;
         double x = (bPrime - b) / (a - aPrime);
